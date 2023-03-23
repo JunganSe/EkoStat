a44b07fc2ba11b881d9458e69bdeeb19a54ecc0b
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-03-23 22:39:39 +0100

    Implementerat unit of work pattern

---
+++ EkoStatApi/EkoStatApi/Data/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using EkoStatApi.Repositories.Interfaces;
+
+namespace EkoStatApi.Data;
+
+public interface IUnitOfWork : IDisposable
+{
+    public IArticleRepository Articles { get; }
+    public IEntryRepository Entries { get; }
+    public ITagRepository Tags { get; }
+    public IUnitRepository Units { get; }
+    public IUserRepository Users { get; }
+
+    Task<int> SaveAsync();
+}
---
+++ EkoStatApi/EkoStatApi/Data/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using EkoStatApi.Repositories;
+using EkoStatApi.Repositories.Interfaces;
+
+namespace EkoStatApi.Data;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly EkoStatContext _ekoStatContext;
+    private IArticleRepository? _articleRepository;
+    private IEntryRepository? _entryRepository;
+    private ITagRepository? _tagRepository;
+    private IUnitRepository? _unitRepository;
+    private IUserRepository? _userRepository;
+
+    public IArticleRepository Articles => _articleRepository ??= new ArticleRepository(_ekoStatContext);
+    public IEntryRepository Entries => _entryRepository ??= new EntryRepository(_ekoStatContext);
+    public ITagRepository Tags => _tagRepository ??= new TagRepository(_ekoStatContext);
+    public IUnitRepository Units => _unitRepository ??= new UnitRepository(_ekoStatContext);
+    public IUserRepository Users => _userRepository ??= new UserRepository(_ekoStatContext);
+
+    public UnitOfWork(EkoStatContext ekoStatContext)
+    {
+        _ekoStatContext = ekoStatContext;
+    }
+
+    public async Task<int> SaveAsync()
+    {
+        return await _ekoStatContext.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        _ekoStatContext.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
