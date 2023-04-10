0fec7f7b8d81ae0f90c324e1f7167fa5d77fc678
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-04-10 18:34:11 +0200

    Automapper för response-dto'er

--- EkoStatApi/EkoStatApi/Data/AutoMapperProfile.cs
+++ EkoStatApi/EkoStatApi/Data/AutoMapperProfile.cs
@@ -8,18 +8,36 @@ public class AutoMapperProfile : AutoMapper.Profile
     public AutoMapperProfile()
     {
         CreateMap<ArticleRequestDto, Article>();
-        CreateMap<Article, ArticleResponseDto>();
+        CreateMap<Article, ArticleResponseDto>()
+            .ForMember(
+                dest => dest.EntryIds,
+                opt => opt.MapFrom(src => src.Entries.Select(e => e.Id)));
 
         CreateMap<EntryRequestDto, Entry>();
         CreateMap<Entry, EntryResponseDto>();
 
         CreateMap<TagRequestDto, Tag>();
-        CreateMap<Tag, TagResponseDto>();
+        CreateMap<Tag, TagResponseDto>()
+            .ForMember(
+                dest => dest.ArticleIds,
+                opt => opt.MapFrom(src => src.Articles.Select(a => a.Id)));
 
         CreateMap<UnitRequestDto, Unit>();
-        CreateMap<Unit, UnitResponseDto>();
+        CreateMap<Unit, UnitResponseDto>()
+            .ForMember(
+                dest => dest.EntryIds,
+                opt => opt.MapFrom(src => src.Entries.Select(e => e.Id)));
 
         CreateMap<UserRequestDto, User>();
-        CreateMap<User, UserResponseDto>();
+        CreateMap<User, UserResponseDto>()
+            .ForMember(
+                dest => dest.TagIds,
+                opt => opt.MapFrom(src => src.Tags.Select(t => t.Id)))
+            .ForMember(
+                dest => dest.ArticleIds,
+                opt => opt.MapFrom(src => src.Articles.Select(a => a.Id)))
+            .ForMember(
+                dest => dest.EntryIds,
+                opt => opt.MapFrom(src => src.Entries.Select(e => e.Id)));
     }
 }
