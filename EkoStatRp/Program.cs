7ee84fb3c84bb5193eecedd5dd57de306b87decb
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-04-17 20:18:28 +0200

    RP: Konfigurerat Session

--- EkoStatRp/Program.cs
+++ EkoStatRp/Program.cs
@@ -9,6 +9,17 @@ public class Program
         // Add services to the container.
         builder.Services.AddRazorPages();
 
+        // För att kunna använda HttpContext.Session-metoder för att lagra data tillfälligt på servern.
+        builder.Services.AddDistributedMemoryCache();
+        builder.Services.AddSession(options =>
+        {
+            //options.IdleTimeout = TimeSpan.FromMinutes(30);
+            //options.Cookie.IsEssential = true; // Gör att cookie inte kan raderas.
+            options.Cookie.HttpOnly = true; // Ska vara med av säkerhetsskäl.
+            options.Cookie.SecurePolicy = CookieSecurePolicy.Always; // Skicka endast cookie över https.
+        });
+        builder.Services.AddHttpContextAccessor(); // För att kunna injecta IHttpContextAccessor, som används för att nå HttpContext.Session-metoder.
+
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
@@ -21,7 +32,7 @@ public class Program
 
         app.UseHttpsRedirection();
         app.UseStaticFiles();
-
+        app.UseSession(); // För att kunna använda HttpContext.Session-metoder för att lagra data tillfälligt på servern.
         app.UseRouting();
 
         app.UseAuthorization();
