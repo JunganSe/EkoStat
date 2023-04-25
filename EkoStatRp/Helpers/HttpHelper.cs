23b06cc32b5b7c29f9924ae24f4243e6b62af2cf
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-04-25 22:46:58 +0200

    RP: Hantering av SessionData i HttpHelper

--- EkoStatRp/Helpers/HttpHelper.cs
+++ EkoStatRp/Helpers/HttpHelper.cs
@@ -3,14 +3,30 @@
 public class HttpHelper
 {
     private readonly IConfiguration _configuration;
+    private readonly IHttpContextAccessor _httpContextAccessor;
 
-    public HttpHelper(IConfiguration configuration)
+    public HttpHelper(IConfiguration configuration, IHttpContextAccessor httpContextAccessor)
     {
         _configuration = configuration;
+        _httpContextAccessor = httpContextAccessor;
     }
 
     public string GetApiUrl()
     {
         return _configuration.GetValue<string>(Constants.AppsettingsJsonNames.ApiUrl);
+    }
+
+    public void SetSessionData(string key, string value)
+    {
+        if (_httpContextAccessor.HttpContext == null)
+            throw new NullReferenceException(nameof(HttpContext));
+        _httpContextAccessor.HttpContext.Session.SetString(key, value);
+    }
+
+    public string? GetSessionData(string key)
+    {
+        if (_httpContextAccessor.HttpContext == null)
+            throw new NullReferenceException(nameof(HttpContext));
+        return _httpContextAccessor.HttpContext?.Session.GetString(key);
     }
 }
