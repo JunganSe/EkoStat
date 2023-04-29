419030541721a88c0631837650413daaee38344e
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-04-29 15:40:45 +0200

    RP: Skapat grund för UserHelper

---
+++ EkoStatRp/Helpers/UserHelper.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace EkoStatRp.Helpers;
+
+public class UserHelper
+{
+    public bool IsLoggedIn(ClaimsPrincipal user)
+    {
+        throw new NotImplementedException();
+    }
+}
--- EkoStatRp/Program.cs
+++ EkoStatRp/Program.cs
@@ -24,6 +24,7 @@ public class Program
         builder.Services.AddHttpContextAccessor(); // För att kunna injecta IHttpContextAccessor, som används för att nå HttpContext.Session-metoder.
 
         builder.Services.AddTransient<HttpHelper>();
+        builder.Services.AddScoped<UserHelper>();
 
         var app = builder.Build();
         // --- Configure the HTTP request pipeline. ---
