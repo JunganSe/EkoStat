88f48c72a4218671fdbd732a46845ba47579ff48
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-05-02 15:19:34 +0200

    RP: Visa taggar för artikel

--- EkoStatRp/Helpers/DtoHelper.cs
+++ EkoStatRp/Helpers/DtoHelper.cs
@@ -4,4 +4,9 @@ namespace EkoStatRp.Helpers;
 
 public class DtoHelper
 {
+    public string GetTagNamesAsString(List<TagResponseDto> tags)
+    {
+        var names = tags.Select(t => t.Name);
+        return string.Join(", ", names);
+    }
 }
