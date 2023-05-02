48e2306d98be6a144d76b86dc2b278a83434a45d
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-05-02 18:48:18 +0200

    RP: Hjälpmetod för att omvandla StringValues till List<int>

--- EkoStatRp/Helpers/DtoHelper.cs
+++ EkoStatRp/Helpers/DtoHelper.cs
@@ -1,4 +1,5 @@
 ﻿using EkoStatLibrary.Dtos;
+using Microsoft.Extensions.Primitives;
 
 namespace EkoStatRp.Helpers;
 
@@ -8,5 +9,17 @@ public class DtoHelper
     {
         var names = tags.Select(t => t.Name);
         return string.Join(", ", names);
+    }
+
+    // Hoppar över de som inte kunde konverteras.
+    public List<int> ParseValidToInt(StringValues strings)
+    {
+        var output = new List<int>();
+        foreach (var s in strings)
+        {
+            if (int.TryParse(s, out int parsedValue))
+                output.Add(parsedValue);
+        }
+        return output;
     }
 }
