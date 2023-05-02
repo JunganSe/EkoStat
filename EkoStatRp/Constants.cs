182a3ac0e96c7fe1f2181cfa26768ceb8a37fe7c
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-05-02 11:35:19 +0200

    RP: Se artiklar

--- EkoStatRp/Constants.cs
+++ EkoStatRp/Constants.cs
@@ -20,6 +20,8 @@ internal static class Constants
         public const string ArticlesByUser = "/Articles/ByUser";
         public const string ArticleCreate = "/Articles";
 
+        public const string EntriesByUser = "/Entries/ByUser";
+
         public const string TagsByUser = "/Tags/Minimal/ByUser";
         public const string TagCreate = "/Tags";
     }
--- EkoStatRp/Pages/Entries/Index.cshtml.cs
+++ EkoStatRp/Pages/Entries/Index.cshtml.cs
@@ -1,16 +1,37 @@
+using EkoStatLibrary.Dtos;
 using EkoStatRp.Common;
 using EkoStatRp.Helpers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EkoStatRp.Pages.Entries;
 
+[BindProperties]
 public class IndexModel : PageModelBase<IndexModel>
 {
+    public List<EntryResponseDto> Entries { get; set; } = new();
+
     public IndexModel(HttpHelper httpHelper, UserHelper userHelper, ILogger<IndexModel> logger)
         : base(httpHelper, userHelper, logger)
     {
     }
 
-    public void OnGet()
+    public async Task<IActionResult> OnGetAsync()
     {
+        try
+        {
+            if (!IsLoggedIn())
+                return GoHome();
+
+            using var httpClient = new HttpClient();
+            var userId = GetUserId();
+            string url = $"{_apiUrl}{Constants.ApiEndpoints.EntriesByUser}/{userId}";
+            Entries = await httpClient.GetFromJsonAsync<List<EntryResponseDto>>(url) ?? new();
+        }
+        catch (Exception ex)
+        {
+            // TODO: Meddela användaren.
+            _logger.LogError(ex, "Fail: Get Entries.");
+        }
+        return Page();
     }
 }
