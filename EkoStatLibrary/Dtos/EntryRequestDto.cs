4e80944493f4a9b47df9300a60420f46474ac317
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-05-04 19:38:22 +0200

    CL: Begränsningar och felmeddelanden i EntryRequestDto

--- EkoStatLibrary/Dtos/EntryRequestDto.cs
+++ EkoStatLibrary/Dtos/EntryRequestDto.cs
@@ -4,22 +4,30 @@ namespace EkoStatLibrary.Dtos;
 
 public class EntryRequestDto
 {
+    [Display(Name = "Comment (optional)")]
     [MaxLength(200)]
     public string? Comment { get; set; }
 
     [Required]
+    [Display(Name = "Timestamp")]
     public DateTimeOffset TimeStamp { get; set; }
 
     [Required]
+    [Display(Name = "Count")]
+    [Range(1, 1000000, ErrorMessage = "Must be {1} to {2}")]
     public double Count { get; set; }
 
     [Required]
+    [Display(Name = "Cost per article")]
+    [Range(typeof(decimal), "0", "1000000", ErrorMessage = "Must be {1} to {2}")]
     public decimal CostPerArticle { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Required")]
+    [Display(Name = "Article")]
     public int ArticleId { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Required")]
+    [Display(Name = "Unit")]
     public int UnitId { get; set; }
 
     [Required]
