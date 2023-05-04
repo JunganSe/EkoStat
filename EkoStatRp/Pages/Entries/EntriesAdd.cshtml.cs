edcd7dc15f8170d36873534758d32a0cebf33ff9
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-05-04 13:45:57 +0200

    RP: Skapa nytt entry (väldigt grundläggande)

--- EkoStatRp/Pages/Entries/EntriesAdd.cshtml.cs
+++ EkoStatRp/Pages/Entries/EntriesAdd.cshtml.cs
@@ -1,16 +1,68 @@
+using EkoStatLibrary.Dtos;
 using EkoStatRp.Common;
 using EkoStatRp.Helpers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EkoStatRp.Pages.Entries;
 
+[BindProperties]
 public class EntriesAdd : PageModelBase<EntriesAdd>
 {
-    public EntriesAdd(HttpHelper httpHelper, UserHelper userHelper, ILogger<EntriesAdd> logger)
+    private readonly DtoHelper _dtoHelper;
+
+    public EntryRequestDto NewEntry { get; set; } = new();
+    public DateTime NewEntryTimeStamp { get; set; }
+    public List<ArticleResponseDto> Articles { get; set; } = new();
+    public List<UnitResponseDto> Units { get; set; } = new();
+
+    public EntriesAdd(HttpHelper httpHelper, UserHelper userHelper, DtoHelper dtoHelper, ILogger<EntriesAdd> logger)
         : base(httpHelper, userHelper, logger)
     {
+        _dtoHelper = dtoHelper;
     }
 
-    public void OnGet()
+    public async Task<IActionResult> OnGetAsync()
     {
+        try
+        {
+            if (!IsLoggedIn())
+                return GoHome();
+
+            using var httpClient = new HttpClient();
+            var userId = GetUserId();
+
+            NewEntryTimeStamp = DateTime.Today; // TODO: Använd timestamp från senaste entry, om den entryn är minre än en timme gammal.
+
+            string articlesUrl = $"{_apiUrl}{LibraryConstants.ApiEndpoints.ArticlesByUser}/{userId}";
+            Articles = await httpClient.GetFromJsonAsync<List<ArticleResponseDto>>(articlesUrl) ?? new();
+
+            string unitsUrl = $"{_apiUrl}{LibraryConstants.ApiEndpoints.UnitsAll}";
+            Units = await httpClient.GetFromJsonAsync<List<UnitResponseDto>>(unitsUrl) ?? new();
+        }
+        catch (Exception ex)
+        {
+            // TODO: Meddela användaren.
+            _logger.LogError(ex, "Fail: Get stuff needed to create entry.");
+        }
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        try
+        {
+            using var httpClient = new HttpClient();
+            string url = _apiUrl + LibraryConstants.ApiEndpoints.EntryCreate;
+            NewEntry.TimeStamp = _dtoHelper.GetDateTimeOffset(NewEntryTimeStamp);
+            NewEntry.UserId = GetUserId();
+            var response = await httpClient.PostAsJsonAsync(url, NewEntry);
+            response.EnsureSuccessStatusCode();
+        }
+        catch (Exception ex)
+        {
+            // TODO: Meddela användaren.
+            _logger.LogError(ex, "Fail: Create entry.");
+        }
+        return RedirectToPage();
     }
 }
