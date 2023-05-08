d51b65922ab29695ee135a508e0a6b46be4546f2
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-05-08 14:56:51 +0200

    RP: Metoder för hantering av TempData (ligger i PageModelBase tills vidare)

--- EkoStatRp/Common/PageModelBase.cs
+++ EkoStatRp/Common/PageModelBase.cs
@@ -3,26 +3,29 @@ using EkoStatRp.Helpers;
 using EkoStatRp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.Json;
 
 namespace EkoStatRp.Common;
 
-public class PageModelBase<T> : PageModel
+public class PageModelBase<TPageModel> : PageModel
 {
     protected readonly HttpHelper _httpHelper;
     protected readonly UserHelper _userHelper;
     protected readonly DtoHelper _dtoHelper;
-    protected readonly ILogger<T> _logger;
+    protected readonly ILogger<TPageModel> _logger;
     protected readonly string _apiUrl;
+    private readonly JsonSerializerOptions _jsonOptions;
 
     public MessageBox MessageBox { get; set; } = new MessageBox("");
 
-    public PageModelBase(HttpHelper httpHelper, UserHelper userHelper, DtoHelper dtoHelper, ILogger<T> logger)
+    public PageModelBase(HttpHelper httpHelper, UserHelper userHelper, DtoHelper dtoHelper, ILogger<TPageModel> logger)
     {
         _httpHelper = httpHelper;
         _userHelper = userHelper;
         _dtoHelper = dtoHelper;
         _logger = logger;
         _apiUrl = _httpHelper.GetApiUrl();
+        _jsonOptions = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
     }
 
     protected int GetUserId()
@@ -45,5 +48,26 @@ public class PageModelBase<T> : PageModel
     {
         string page = Constants.RazorPages.Login;
         return new RedirectToPageResult(page);
+    }
+
+    protected void SetTempData(string key, object value)
+    {
+        string json = JsonSerializer.Serialize(value, _jsonOptions);
+        TempData[key] = json;
+    }
+
+    protected T GetTempData<T>(string key, bool keepData = true)
+    {
+        try
+        {
+            string json = (keepData) 
+                ? (string)(TempData.Peek(key) ?? "{}")
+                : (string)(TempData[key] ?? "{}");
+            return JsonSerializer.Deserialize<T>(json, _jsonOptions)!;
+        }
+        catch (Exception ex)
+        {
+            throw new JsonException($"Failed to deserialize 'TempData[{key}]' to type '{typeof(T)}'.", ex);
+        }
     }
 }
