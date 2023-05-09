5908ce6b14a57fa1e4a5cd98f52ad14b7be83f0a
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-05-09 09:45:58 +0200

    RP: Filter på entries-sidan

--- EkoStatRp/Pages/Entries/EntriesIndex.cshtml.cs
+++ EkoStatRp/Pages/Entries/EntriesIndex.cshtml.cs
@@ -2,6 +2,7 @@ using EkoStatLibrary.Dtos;
 using EkoStatLibrary.Helpers;
 using EkoStatRp.Common;
 using EkoStatRp.Helpers;
+using EkoStatRp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EkoStatRp.Pages.Entries;
@@ -9,6 +10,7 @@ namespace EkoStatRp.Pages.Entries;
 [BindProperties]
 public class EntriesIndex : PageModelBase<EntriesIndex>
 {
+    public EntriesFilterViewModel FilterViewModel { get; set; } = new();
     public List<EntryGroup> EntryGroups { get; set; } = new();
 
     public EntriesIndex(HttpHelper httpHelper, UserHelper userHelper, DtoHelper dtoHelper, ILogger<EntriesIndex> logger)
@@ -25,6 +27,15 @@ public class EntriesIndex : PageModelBase<EntriesIndex>
 
             using var httpClient = new HttpClient();
             var userId = GetUserId();
+
+            string articlesUrl = $"{_apiUrl}{LibraryConstants.ApiEndpoints.ArticlesByUser}/{userId}";
+            FilterViewModel.Articles = await httpClient.GetFromJsonAsync<List<ArticleResponseDto>>(articlesUrl) ?? new();
+            SetTempData(nameof(FilterViewModel.Articles), FilterViewModel.Articles);
+
+            string tagsUrl = $"{_apiUrl}{LibraryConstants.ApiEndpoints.TagsByUser}/{userId}";
+            FilterViewModel.Tags = await httpClient.GetFromJsonAsync<List<TagResponseDto>>(tagsUrl) ?? new();
+            SetTempData(nameof(FilterViewModel.Tags), FilterViewModel.Tags);
+
             string url = $"{_apiUrl}{LibraryConstants.ApiEndpoints.EntriesByUser}/{userId}";
             var entries = await httpClient.GetFromJsonAsync<List<EntryResponseDto>>(url) ?? new();
             EntryGroups = _dtoHelper.GroupEntries(entries);
@@ -35,5 +46,36 @@ public class EntriesIndex : PageModelBase<EntriesIndex>
             _logger.LogError(ex, "Fail: Get Entries.");
         }
         return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        try
+        {
+            using var httpClient = new HttpClient();
+            var userId = GetUserId();
+
+            var articleIds = Request.Form["articleIds"].ToList();
+            FilterViewModel.Filter.ArticleIds = _dtoHelper.ParseStringsToInts(articleIds);
+
+            var tagIds = Request.Form["tagIds"].ToList();
+            FilterViewModel.Filter.TagIds = _dtoHelper.ParseStringsToInts(tagIds);
+
+            string url = $"{_apiUrl}{LibraryConstants.ApiEndpoints.EntriesFiltered}/{userId}";
+            var response = await httpClient.PostAsJsonAsync(url, FilterViewModel.Filter);
+            response.EnsureSuccessStatusCode();
+            var entries = await response.Content.ReadFromJsonAsync<List<EntryResponseDto>>() ?? new();
+            EntryGroups = _dtoHelper.GroupEntries(entries);
+
+            FilterViewModel.Articles = GetTempData<List<ArticleResponseDto>>(nameof(FilterViewModel.Articles));
+            FilterViewModel.Tags = GetTempData<List<TagResponseDto>>(nameof(FilterViewModel.Tags));
+            return Page();
+        }
+        catch (Exception ex)
+        {
+            // TODO: Meddela användaren.
+            _logger.LogError(ex, "Fail: Generate report.");
+            return RedirectToPage();
+        }
     }
 }
