40c80f4c135ab7ccaf0199bf3e60557d1b9c0870
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-05-09 11:50:23 +0200

    CL: Metoder för hämtning i ApiHandler

--- EkoStatLibrary/Helpers/ApiHandler.cs
+++ EkoStatLibrary/Helpers/ApiHandler.cs
@@ -1,4 +1,7 @@
-﻿namespace EkoStatLibrary.Helpers;
+﻿using EkoStatLibrary.Dtos;
+using System.Net.Http.Json;
+
+namespace EkoStatLibrary.Helpers;
 
 public class ApiHandler
 {
@@ -7,5 +10,46 @@ public class ApiHandler
     public ApiHandler(string apiUrl)
     {
         _apiUrl = apiUrl;
+    }
+
+    private HttpClient GetHttpClient()
+        => new HttpClient() { BaseAddress = new Uri(_apiUrl) };
+
+    private async Task<T?> GetAsync<T>(string endpoint)
+    {
+        using var httpClient = GetHttpClient();
+        return await httpClient.GetFromJsonAsync<T>(endpoint);
+    }
+
+
+
+    public async Task<List<TagResponseDto>> GetTagsByUserAsync(int userId)
+    {
+        string endpoint = Constants.ApiEndpoints.TagsByUser + userId;
+        return await GetAsync<List<TagResponseDto>>(endpoint) ?? new();
+    }
+
+    public async Task<List<ArticleResponseDto>> GetArticlesByUserAsync(int userId)
+    {
+        string endpoint = Constants.ApiEndpoints.ArticlesByUser + userId;
+        return await GetAsync<List<ArticleResponseDto>>(endpoint) ?? new();
+    }
+
+    public async Task<List<EntryResponseDto>> GetEntriesByUserAsync(int userId)
+    {
+        string endpoint = Constants.ApiEndpoints.EntriesByUser + userId;
+        return await GetAsync<List<EntryResponseDto>>(endpoint) ?? new();
+    }
+
+    public async Task<EntryResponseDto> GetLatestEntryByUserAsync(int userId)
+    {
+        string endpoint = Constants.ApiEndpoints.EntryLatestByUser + userId;
+        return await GetAsync<EntryResponseDto>(endpoint) ?? new();
+    }
+
+    public async Task<List<UnitResponseDto>> GetAllUnitsAsync()
+    {
+        string endpoint = Constants.ApiEndpoints.UnitsAll;
+        return await GetAsync<List<UnitResponseDto>>(endpoint) ?? new();
     }
 }
