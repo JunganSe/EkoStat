651bbb34d8710b0b8bb2ec54d039e92716f31161
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-05-09 14:43:09 +0200

    CL: Post-metoder i ApiHandler

--- EkoStatLibrary/Helpers/ApiHandler.cs
+++ EkoStatLibrary/Helpers/ApiHandler.cs
@@ -27,6 +27,14 @@ public class ApiHandler
         return await httpClient.GetFromJsonAsync<T>(endpoint);
     }
 
+    private async Task<HttpResponseMessage> PostAsync<T>(string endpoint, T entity)
+    {
+        using var httpClient = GetHttpClient();
+        var response = await httpClient.PostAsJsonAsync(endpoint, entity);
+        response.EnsureSuccessStatusCode();
+        return response;
+    }
+
 
 
     #region Entries
@@ -41,6 +49,19 @@ public class ApiHandler
         string endpoint = Constants.ApiEndpoints.EntryLatestByUser + userId;
         return await GetAsync<EntryResponseDto>(endpoint) ?? new();
     }
+
+    public async Task<List<EntryResponseDto>> GetEntriesFilteredAsync(int userId, EntryFilterRequestDto filter)
+    {
+        string endpoint = Constants.ApiEndpoints.EntriesFiltered + userId;
+        var response = await PostAsync(endpoint, filter);
+        return await response.Content.ReadFromJsonAsync<List<EntryResponseDto>>() ?? new();
+    }
+
+    public async Task<HttpResponseMessage> CreateEntryAsync(EntryRequestDto entry)
+    {
+        string endpoint = Constants.ApiEndpoints.EntryCreate;
+        return await PostAsync(endpoint, entry);
+    }
     #endregion
 
     #region Articles
@@ -49,6 +70,12 @@ public class ApiHandler
         string endpoint = Constants.ApiEndpoints.ArticlesByUser + userId;
         return await GetAsync<List<ArticleResponseDto>>(endpoint) ?? new();
     }
+
+    public async Task<HttpResponseMessage> CreateArticleAsync(ArticleRequestDto article)
+    {
+        string endpoint = Constants.ApiEndpoints.ArticleCreate;
+        return await PostAsync(endpoint, article);
+    }
     #endregion
 
     #region Tags
@@ -56,6 +83,12 @@ public class ApiHandler
     {
         string endpoint = Constants.ApiEndpoints.TagsByUser + userId;
         return await GetAsync<List<TagResponseDto>>(endpoint) ?? new();
+    }
+
+    public async Task<HttpResponseMessage> CreateTagAsync(TagRequestDto tag)
+    {
+        string endpoint = Constants.ApiEndpoints.TagCreate;
+        return await PostAsync(endpoint, tag);
     }
     #endregion
 
@@ -66,9 +99,4 @@ public class ApiHandler
         return await GetAsync<List<UnitResponseDto>>(endpoint) ?? new();
     }
     #endregion
-
-
-
-
-
 }
