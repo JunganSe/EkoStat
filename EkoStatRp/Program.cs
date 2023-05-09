4e01853754285548d1ead2e4dcf28f312b9df849
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-05-09 13:52:00 +0200

    RP: Bytt livslängd på DI-services

--- EkoStatRp/Program.cs
+++ EkoStatRp/Program.cs
@@ -24,8 +24,8 @@ public class Program
         });
         builder.Services.AddHttpContextAccessor(); // För att kunna injecta IHttpContextAccessor, som används för att nå HttpContext.Session-metoder.
 
-        builder.Services.AddTransient<DtoHelper>();
-        builder.Services.AddTransient<HttpHelper>();
+        builder.Services.AddScoped<DtoHelper>();
+        builder.Services.AddScoped<HttpHelper>();
         builder.Services.AddScoped<UserHelper>();
         string apiUrl = builder.Configuration.GetValue<string>(Constants.AppsettingsKeys.ApiUrl);
         builder.Services.AddScoped(sp => new ApiHandler(apiUrl));
