3913f1dd348fb36731ff5b9a4444447d3c894314
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-05-10 12:02:31 +0200

    CL: Ny constructor och validering på EntryGroupByArticle

--- EkoStatLibrary/DtoContainers/EntryGroupByArticle.cs
+++ EkoStatLibrary/DtoContainers/EntryGroupByArticle.cs
@@ -6,11 +6,23 @@ public class EntryGroupByArticle
 {
     public decimal CostTotal => Entries.Sum(e => e.CostPerArticle * e.Count);
 
-    public ArticleResponseDto Article { get; private set; }
+    public ArticleResponseDto Article { get; private set; } = null!;
     public List<EntryResponseDto> Entries { get; set; } = new();
 
-    public EntryGroupByArticle(ArticleResponseDto article)
+    public EntryGroupByArticle(List<EntryResponseDto> entries)
     {
-        Article = article;
+        if (entries.Count > 0)
+        {
+            EnsureSameArticle(entries);
+            Article = entries[0].Article;
+            Entries = entries;
+        }
+    }
+
+    private void EnsureSameArticle(List<EntryResponseDto> entries)
+    {
+        var firstArticleId = entries.First().Article.Id;
+        if (!entries.All(e => e.Article.Id == firstArticleId))
+            throw new ArgumentException("Not all entries have the same article.");
     }
 }
