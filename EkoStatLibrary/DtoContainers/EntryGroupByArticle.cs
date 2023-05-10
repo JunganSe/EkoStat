bd84a717405ac148ede78ff31f248550962cc607
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-05-10 11:01:27 +0200

    CL: Skapat EntryGroupByArticle

---
+++ EkoStatLibrary/DtoContainers/EntryGroupByArticle.cs
@@ -0,0 +1,16 @@
+﻿using EkoStatLibrary.Dtos;
+
+namespace EkoStatLibrary.DtoContainers;
+
+public class EntryGroupByArticle
+{
+    public decimal CostTotal => Entries.Sum(e => e.CostPerArticle * e.Count);
+
+    public ArticleResponseDto Article { get; private set; }
+    public List<EntryResponseDto> Entries { get; set; } = new();
+
+    public EntryGroupByArticle(ArticleResponseDto article)
+    {
+        Article = article;
+    }
+}
