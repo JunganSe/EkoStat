7d757bb6990c249bc967dfa8be992de9a62529e2
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-05-12 17:50:20 +0200

    CL: Flera extension methods accepterar IEnumerable istället för List

--- EkoStatLibrary/Extensions/DtoExtensions.cs
+++ EkoStatLibrary/Extensions/DtoExtensions.cs
@@ -5,44 +5,44 @@ namespace EkoStatLibrary.Extensions.DtoExtensions;
 
 public static class EntryGroupExtensions
 {
-    public static DateTime? GetEarliestTimestamp(this List<EntryGroupByTimestamp> entryGroups) 
+    public static DateTime? GetEarliestTimestamp(this IEnumerable<EntryGroupByTimestamp> entryGroups) 
         => (entryGroups.Any())
             ? entryGroups.Select(e => e.Timestamp).Min()
             : null;
-    public static DateTime? GetEarliestTimestamp(this List<EntryGroupByArticle> entryGroups) 
+    public static DateTime? GetEarliestTimestamp(this IEnumerable<EntryGroupByArticle> entryGroups) 
         => (entryGroups.Any())
             ? entryGroups.SelectMany(eg => eg.Entries).Select(e => e.Timestamp).Min()
             : null;
 
-    public static DateTime? GetLatestTimestamp(this List<EntryGroupByTimestamp> entryGroups) 
+    public static DateTime? GetLatestTimestamp(this IEnumerable<EntryGroupByTimestamp> entryGroups) 
         => (entryGroups.Any())
             ? entryGroups.Select(e => e.Timestamp).Max()
             : null;
-    public static DateTime? GetLatestTimestamp(this List<EntryGroupByArticle> entryGroups) 
+    public static DateTime? GetLatestTimestamp(this IEnumerable<EntryGroupByArticle> entryGroups) 
         => (entryGroups.Any())
             ? entryGroups.SelectMany(eg => eg.Entries).Select(e => e.Timestamp).Max()
             : null;
 
-    public static int GetEntriesCount(this List<EntryGroupByTimestamp> entryGroups)
+    public static int GetEntriesCount(this IEnumerable<EntryGroupByTimestamp> entryGroups)
         => entryGroups.Sum(eg => eg.Entries.Count);
-    public static int GetEntriesCount(this List<EntryGroupByArticle> entryGroups)
+    public static int GetEntriesCount(this IEnumerable<EntryGroupByArticle> entryGroups)
         => entryGroups.Sum(eg => eg.Entries.Count);
 
-    public static decimal GetTotalCost(this List<EntryGroupByTimestamp> entryGroups)
+    public static decimal GetTotalCost(this IEnumerable<EntryGroupByTimestamp> entryGroups)
         => entryGroups.Sum(eg => eg.Entries.Sum(e => e.CostPerArticle * e.Count));
-    public static decimal GetTotalCost(this List<EntryGroupByArticle> entryGroups)
+    public static decimal GetTotalCost(this IEnumerable<EntryGroupByArticle> entryGroups)
         => entryGroups.Sum(eg => eg.Entries.Sum(e => e.CostPerArticle * e.Count));
 }
 
 public static class TagExtensions
 {
-    public static string GetNamesAsString(this List<TagResponseDto> tags)
+    public static string GetNamesAsString(this IEnumerable<TagResponseDto> tags)
         => string.Join(", ", tags.Select(t => t.Name));
 }
 
 public static class EntryExtensions
 {
-    public static List<EntryGroupByTimestamp> GroupByTimestamp(this List<EntryResponseDto> entries)
+    public static List<EntryGroupByTimestamp> GroupByTimestamp(this IEnumerable<EntryResponseDto> entries)
     {
         var distinctTimestamps = entries
             .Select(e => e.Timestamp)
@@ -59,7 +59,7 @@ public static class EntryExtensions
         return groups.OrderByDescending(e => e.Timestamp).ToList();
     }
 
-    public static List<EntryGroupByArticle> GroupByArticle(this List<EntryResponseDto> entries)
+    public static List<EntryGroupByArticle> GroupByArticle(this IEnumerable<EntryResponseDto> entries)
     {
         var distinctArticles = entries
             .GroupBy(e => e.Article.Id)
