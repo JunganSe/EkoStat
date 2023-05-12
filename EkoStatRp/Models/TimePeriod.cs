e0e41cd963e0ed8909eac505e4755ccdb4dc8b91
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-05-12 15:36:56 +0200

    RP: TimePeriod använder NodaTime

--- EkoStatRp/Models/TimePeriod.cs
+++ EkoStatRp/Models/TimePeriod.cs
@@ -1,22 +1,26 @@
-﻿namespace EkoStatRp.Models;
+﻿using NodaTime;
+
+namespace EkoStatRp.Models;
 
 public class TimePeriod
 {
-    private readonly int _roundingPrecision = 2;
+    private readonly Period _nodaPeriod;
 
     public DateTime Start { get; set; }
     public DateTime End { get; set; }
 
     public TimeSpan Duration => End - Start;
-    public double Days => Math.Round(Duration.TotalDays, _roundingPrecision);
-    public double Weeks => Math.Round(Days / 7, _roundingPrecision);
-    public double Months => Math.Round(Days / 30.44, _roundingPrecision);
-    public double Years => Math.Round(Days / 365.25, _roundingPrecision);
+    public int Years => _nodaPeriod.Years;
+    public int Months => _nodaPeriod.Months;
+    public int Days => _nodaPeriod.Days;
 
     public TimePeriod(DateTime start, DateTime end)
     {
         Start = start;
         End = end;
+        var nodaStart = new LocalDateTime(start.Year, start.Month, start.Day, start.Hour, start.Minute, start.Second);
+        var nodaEnd = new LocalDateTime(end.Year, end.Month, end.Day, end.Hour, end.Minute, end.Second);
+        _nodaPeriod = Period.Between(nodaStart, nodaEnd);
     }
 
 }
