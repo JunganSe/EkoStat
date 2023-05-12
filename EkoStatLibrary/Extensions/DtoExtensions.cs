2137c09d630d9e38e86e95b6c3981af11564df6e
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-05-12 10:00:13 +0200

    CL: Skapat extension methods för entry-grupper

---
+++ EkoStatLibrary/Extensions/DtoExtensions.cs
@@ -0,0 +1,34 @@
+﻿using EkoStatLibrary.DtoContainers;
+
+namespace EkoStatLibrary.Extensions.DtoExtensions;
+
+public static class EntryGroupExtensions
+{
+    public static DateTime? GetEarliestTimestamp(this List<EntryGroupByTimestamp> entryGroups) 
+        => (entryGroups.Any())
+            ? entryGroups.Select(e => e.Timestamp).Min()
+            : null;
+    public static DateTime? GetEarliestTimestamp(this List<EntryGroupByArticle> entryGroups) 
+        => (entryGroups.Any())
+            ? entryGroups.SelectMany(eg => eg.Entries).Select(e => e.Timestamp).Min()
+            : null;
+
+    public static DateTime? GetLatestTimestamp(this List<EntryGroupByTimestamp> entryGroups) 
+        => (entryGroups.Any())
+            ? entryGroups.Select(e => e.Timestamp).Max()
+            : null;
+    public static DateTime? GetLatestTimestamp(this List<EntryGroupByArticle> entryGroups) 
+        => (entryGroups.Any())
+            ? entryGroups.SelectMany(eg => eg.Entries).Select(e => e.Timestamp).Max()
+            : null;
+
+    public static int GetEntriesCount(this List<EntryGroupByTimestamp> entryGroups)
+        => entryGroups.Sum(eg => eg.Entries.Count);
+    public static int GetEntriesCount(this List<EntryGroupByArticle> entryGroups)
+        => entryGroups.Sum(eg => eg.Entries.Count);
+
+    public static decimal GetTotalCost(this List<EntryGroupByTimestamp> entryGroups)
+        => entryGroups.Sum(eg => eg.Entries.Sum(e => e.CostPerArticle * e.Count));
+    public static decimal GetTotalCost(this List<EntryGroupByArticle> entryGroups)
+        => entryGroups.Sum(eg => eg.Entries.Sum(e => e.CostPerArticle * e.Count));
+}
