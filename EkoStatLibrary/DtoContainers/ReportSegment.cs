095209f064fd1b5397aa2dcdfab8105ce3f0cd8b
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-05-12 16:52:42 +0200

    CL: Skapat ReportSegment som håller grupperade entries under en viss tidsperiod

---
+++ EkoStatLibrary/DtoContainers/ReportSegment.cs
@@ -0,0 +1,21 @@
+﻿using EkoStatLibrary.Dtos;
+using EkoStatLibrary.Helpers;
+using EkoStatLibrary.Models;
+
+namespace EkoStatLibrary.DtoContainers;
+
+public class ReportSegment
+{
+    public TimePeriod TimePeriod { get; init; }
+    public List<EntryGroupByArticle> EntryGroupsByArticle { get; init; }
+
+    public ReportSegment(TimePeriod timePeriod, List<EntryResponseDto> entries)
+    {
+        TimePeriod = timePeriod;
+        var relevantEntries = entries
+            .Where(e => e.Timestamp >= TimePeriod.Start)
+            .Where(e => e.Timestamp < TimePeriod.End)
+            .ToList();
+        EntryGroupsByArticle = new DtoHelper().GroupEntriesByArticle(relevantEntries);
+    }
+}
