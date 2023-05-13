e8f42e7143d0b08b12d9069c209b64faa85eb857
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-05-13 23:06:56 +0200

    CL: Skapat TimeHelper.GetTimePeriodsByWeek() och tillhörande unit test

--- EkoStatLibrary/Helpers/TimeHelper.cs
+++ EkoStatLibrary/Helpers/TimeHelper.cs
@@ -4,5 +4,33 @@ namespace EkoStatLibrary.Helpers;
 
 public class TimeHelper
 {
+    public List<TimePeriod> GetTimePeriodsByWeek(DateTime start, DateTime end, DayOfWeek firstDayOfWeek = DayOfWeek.Monday)
+    {
+        var timePeriods = new List<TimePeriod>();
+        var startDate = DateOnly.FromDateTime(start);
+        var endDate = DateOnly.FromDateTime(end);
 
+        var weekStartDate = GetFirstDayOfWeekOnOrAfterDate(startDate, firstDayOfWeek);
+
+        if (startDate < weekStartDate)
+            timePeriods.Add(new TimePeriod(startDate, weekStartDate));
+
+        while (weekStartDate.AddDays(7) <= endDate)
+        {
+            timePeriods.Add(new TimePeriod(weekStartDate, weekStartDate.AddDays(7)));
+            weekStartDate = weekStartDate.AddDays(7);
+        }
+
+        if (weekStartDate < endDate)
+            timePeriods.Add(new TimePeriod(weekStartDate, endDate));
+
+        return timePeriods;
+
+        DateOnly GetFirstDayOfWeekOnOrAfterDate(DateOnly date, DayOfWeek firstDayOfWeek)
+        {
+            while (date.DayOfWeek != firstDayOfWeek)
+                date = date.AddDays(1);
+            return date;
+        }
+    }
 }
---
+++ EkoStatLibraryTests/Helpers/TimeHelperTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EkoStatLibrary.Helpers.Tests;
+
+[TestClass()]
+public class TimeHelperTests
+{
+    [TestMethod()]
+    [DataRow("2023-05-01", "2023-05-07", DayOfWeek.Monday, 1)]
+    [DataRow("2023-05-01", "2023-05-08", DayOfWeek.Monday, 1)]
+    [DataRow("2023-05-01", "2023-05-09", DayOfWeek.Monday, 2)]
+    [DataRow("2023-04-30", "2023-05-06", DayOfWeek.Monday, 2)]
+    [DataRow("2023-04-30", "2023-05-08", DayOfWeek.Monday, 2)]
+    [DataRow("2023-05-02", "2023-05-08", DayOfWeek.Monday, 1)]
+    [DataRow("2023-05-02", "2023-05-09", DayOfWeek.Monday, 2)]
+    [DataRow("2023-05-02", "2023-05-10", DayOfWeek.Monday, 2)]
+    [DataRow("2023-05-02", "2023-05-14", DayOfWeek.Monday, 2)]
+    [DataRow("2023-05-02", "2023-05-15", DayOfWeek.Monday, 2)]
+    [DataRow("2023-05-02", "2023-05-16", DayOfWeek.Monday, 3)]
+
+    [DataRow("2023-05-01", "2023-05-09", DayOfWeek.Tuesday, 2)]
+    [DataRow("2023-05-01", "2023-05-10", DayOfWeek.Tuesday, 3)]
+    [DataRow("2023-05-01", "2023-05-16", DayOfWeek.Tuesday, 3)]
+    [DataRow("2023-05-01", "2023-05-17", DayOfWeek.Tuesday, 4)]
+
+    [DataRow("2023-05-01", "2023-05-13", DayOfWeek.Sunday, 2)]
+    [DataRow("2023-05-01", "2023-05-14", DayOfWeek.Sunday, 2)]
+    [DataRow("2023-05-01", "2023-05-15", DayOfWeek.Sunday, 3)]
+    public void GetTimePeriodsByCalendarWeekTest(string start, string end, DayOfWeek firstDayOfWeek, int expectedPeriodsCount)
+    {
+        var timeHelper = new TimeHelper();
+        var startDate = DateTime.Parse(start);
+        var endDate = DateTime.Parse(end);
+        var timePeriods = timeHelper.GetTimePeriodsByWeek(startDate, endDate, firstDayOfWeek);
+        Assert.AreEqual(expectedPeriodsCount, timePeriods.Count);
+    }
+}
