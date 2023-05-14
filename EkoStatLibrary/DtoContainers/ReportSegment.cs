ae21a56f64359e6755b6820270de18dd9c159866
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-05-14 13:21:43 +0200

    CL: Bytt namn på property I ReportSegment

--- EkoStatLibrary/DtoContainers/ReportSegment.cs
+++ EkoStatLibrary/DtoContainers/ReportSegment.cs
@@ -7,12 +7,12 @@ namespace EkoStatLibrary.DtoContainers;
 public class ReportSegment
 {
     public TimePeriod TimePeriod { get; init; }
-    public List<EntryGroupByArticle> EntryGroupsByArticle { get; init; }
+    public List<EntryGroupByArticle> EntryGroups { get; init; }
 
     public ReportSegment(TimePeriod timePeriod, List<EntryResponseDto> entries)
     {
         TimePeriod = timePeriod;
-        EntryGroupsByArticle = entries
+        EntryGroups = entries
             .Where(e => e.Timestamp >= TimePeriod.Start)
             .Where(e => e.Timestamp < TimePeriod.End)
             .GroupByArticle();
