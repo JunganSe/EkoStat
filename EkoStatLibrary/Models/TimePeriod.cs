0a1bb608b4f0d1b131be9b89da78be2820cd013c
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-05-14 13:07:30 +0200

    CL: Förbättrat TimePeriod

--- EkoStatLibrary/Models/TimePeriod.cs
+++ EkoStatLibrary/Models/TimePeriod.cs
@@ -4,32 +4,37 @@ namespace EkoStatLibrary.Models;
 
 public class TimePeriod
 {
-    private readonly Period _nodaPeriod;
-
     public DateTime Start { get; init; }
     public DateTime End { get; init; }
 
-    public TimeSpan Duration => End - Start;
-    public int Years => _nodaPeriod.Years;
-    public int Months => _nodaPeriod.Months;
-    public int Days => _nodaPeriod.Days;
+    public int Years { get; private set; }
+    public int Months { get; private set; }
+    public int Days { get; private set; }
+    public int TotalDays { get; private set; }
 
     public TimePeriod(DateTime start, DateTime end)
     {
         Start = start;
         End = end;
-        var nodaStart = LocalDateTime.FromDateTime(start);
-        var nodaEnd = LocalDateTime.FromDateTime(end);
-        _nodaPeriod = Period.Between(nodaStart, nodaEnd);
+        SetProperties();
     }
 
     public TimePeriod(DateOnly start, DateOnly end)
     {
         Start = new DateTime(start.Year, start.Month, start.Day);
         End = new DateTime(end.Year, end.Month, end.Day);
-        var nodaStart = LocalDateTime.FromDateTime(Start);
-        var nodaEnd = LocalDateTime.FromDateTime(End);
-        _nodaPeriod = Period.Between(nodaStart, nodaEnd);
+        SetProperties();
+    }
+
+    private void SetProperties()
+    {
+        var start = LocalDateTime.FromDateTime(Start);
+        var end = LocalDateTime.FromDateTime(End);
+        var period = Period.Between(start, end);
+        Years = period.Years;
+        Months = period.Months;
+        Days = period.Days;
+        TotalDays = Period.Between(start, end, PeriodUnits.Days).Days;
     }
 
 }
