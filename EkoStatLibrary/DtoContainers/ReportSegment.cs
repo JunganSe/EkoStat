655c461367f368f687d63137f6818c9056c9a7f3
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-05-14 13:59:18 +0200

    CL: Uppdaterat ReportSegment så att den kan räkna ut gränsvärde för kostnad

--- EkoStatLibrary/DtoContainers/ReportSegment.cs
+++ EkoStatLibrary/DtoContainers/ReportSegment.cs
@@ -1,4 +1,5 @@
 ﻿using EkoStatLibrary.Dtos;
+using EkoStatLibrary.Enums;
 using EkoStatLibrary.Extensions.DtoExtensions;
 using EkoStatLibrary.Models;
 
@@ -8,6 +9,7 @@ public class ReportSegment
 {
     public TimePeriod TimePeriod { get; init; }
     public List<EntryGroupByArticle> EntryGroups { get; init; }
+    public decimal? CostThreshold { get; private set; }
 
     public ReportSegment(TimePeriod timePeriod, List<EntryResponseDto> entries)
     {
@@ -16,5 +18,31 @@ public class ReportSegment
             .Where(e => e.Timestamp >= TimePeriod.Start)
             .Where(e => e.Timestamp < TimePeriod.End)
             .GroupByArticle();
+    }
+
+    public void SetCostThreshold(decimal? limit, LimitType limitType)
+    {
+        CostThreshold = GetCostThreshold(limit, limitType);
+    }
+
+    private decimal GetCostThreshold(decimal? limit, LimitType limitType)
+    {
+        if (limit == null)
+            return decimal.MaxValue;
+
+        if (limitType == LimitType.Fixed)
+            return (decimal)limit;
+        else if (limitType == LimitType.Percentile)
+            return GetArticleCostPercentileValue((decimal)limit);
+
+        return decimal.MaxValue;
+
+        decimal GetArticleCostPercentileValue(decimal percentile)
+        {
+            decimal lowestCost = EntryGroups.Select(eg => eg.TotalCost).Min();
+            decimal highestCost = EntryGroups.Select(eg => eg.TotalCost).Max();
+            decimal costSpan = highestCost - lowestCost;
+            return lowestCost + costSpan * percentile / 100;
+        }
     }
 }
